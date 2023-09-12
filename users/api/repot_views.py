60344fb1b4918882cc3e258bf945f30ddbab410d
aLiyevTamerlan <tamerlan-aliyev-2021@mail.ru>
2023-09-12 16:04:39 +0400

    add report score result function

--- users/api/repot_views.py
+++ users/api/repot_views.py
@@ -11,41 +11,6 @@ from users.serializers.report_serializers import ReportUploadSerializer
 from users.serializers.user_account_file_serializers import UserAccountFilePage
 from users.helpers.sync_user_helpers import *
 
-
-# class ReportUploadAPIView(APIView):
-#     parser_classes = (MultiPartParser,)
-
-#     def post(self, request, *args, **kwargs):
-#         # print(request.data.get('report_file'))
-#         req_data = request.data.get('report_file')
-#         format, imgstr = req_data.split(';base64,') 
-#         ext = format.split('/')[-1] 
-#         cont_data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-#         # print(req_data)
-#         try:
-#           email = request.data.get('email')
-#           user = UserAccount.objects.get(email=email)
-#         except ReportModel.DoesNotExist:
-#             return Response({'error': 'User not found with the provided email.'}, status=status.HTTP_404_NOT_FOUND)
-#         # print(email,cont_data)
-
-#         data = {'user': user.id, 'report_file': cont_data, 
-#                 'education_score': 0.0,
-#                 'language_score': 0.0,
-#                 'special_skills_score': 0.0,
-#                 'sport_score': 0.0,
-#                 'work_experiance_score': 0.0,
-#                 'program_score': 0.0,
-#                 }
-
-#         file_serializer = ReportUploadSerializer(data=data)
-
-#         if file_serializer.is_valid():
-#             file_serializer.save()
-#             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-#         else:
-#             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class ReportUploadAPIView(APIView):
     def post(self, request, *args, **kwargs):
         try:

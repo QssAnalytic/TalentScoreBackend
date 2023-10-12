f57e82718cb5260c7046864db43aea329a3c0dd3
aLiyevTamerlan <tamerlan-aliyev-2021@mail.ru>
2023-10-12 16:16:53 +0400

    fix erro delete image on s3

--- users/serializers/user_serializers.py
+++ users/serializers/user_serializers.py
@@ -43,7 +43,7 @@ class LoginSerializer(serializers.Serializer):
 class UserAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAccount
-        fields = ("email","first_name","birth_date","last_name","gender", "report_test")
+        fields = ("email","first_name","birth_date","last_name","gender", "report_test", "profile_photo")
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
